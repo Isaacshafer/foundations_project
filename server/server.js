666c8307df9360a413447477f7a7b05e4225dc07
Isaac Shafer <izakshafer@gmail.com>
2022-03-30 11:45:45 -0600

    added endpoints

--- server/server.js
+++ server/server.js
@@ -9,9 +9,38 @@ app.use(express.json())
 app.use(cors())
 const {register, login, score, getCurrentHighscore, getGlobalHighscore} = require('./controller')
 const {seed} = require('./seed')
+const { ppid } = require('process')
 
-app.use(express.static(path.join(__dirname, '../client')))
-app.use(express.static(path.join(__dirname, '../server')))
+// app.use(express.static(path.join(__dirname, '../client')))
+// app.use(express.static(path.join(__dirname, '../server')))
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname, '../client/home.html'))
+})
+app.get('/styles', function(req,res){
+    res.sendFile(path.join(__dirname, '../client/main.css'))
+})
+app.get('/game', function(req,res){
+    res.sendFile(path.join(__dirname, '../client/game.html'))
+})
+app.get('/homejs', function(req,res){
+    res.sendFile(path.join(__dirname, '../client/home.js'))
+})
+app.get('/gamejs', function(req,res){
+    res.sendFile(path.join(__dirname, '../client/food.js'))
+})
+app.get('/gamejs', function(req,res){
+    res.sendFile(path.join(__dirname, '../client/grid.js'))
+})
+app.get('/gamejs', function(req,res){
+    res.sendFile(path.join(__dirname, '../client/input.js'))
+})
+app.get('/gamejs', function(req,res){
+    res.sendFile(path.join(__dirname, '../client/main.js'))
+})
+app.get('/gamejs', function(req,res){
+    res.sendFile(path.join(__dirname, '../client/snake.js'))
+})
+
 
 app.post('/seed', seed)
 app.get('/currenthighscore', getCurrentHighscore)
